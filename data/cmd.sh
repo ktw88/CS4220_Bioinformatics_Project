4e1ae4b38ee25e63439c73bb01f1a4ae653e8e2a
kartong <kartongw@gmail.com>
2017-03-15 12:44:23 +0800

    script to filter out high expression data

---
+++ data/cmd.sh
@@ -0,0 +1,2 @@
+perl filterExpr.pl Normalization.BRCA.txt 1 10 > Normalization.BRCA.minAveExpr1.maxExpr10.txt
+perl filterExpr.pl Normalization.BRCA.txt 10 50 > Normalization.BRCA.minAveExpr10.maxExpr50.txt
---
+++ data/filterExpr.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+my $inputFile = $ARGV[0];
+my $aveExprAtLeast = $ARGV[1];
+my $maxExprAtLeast = $ARGV[2];
+
+open(my $EXPR, $inputFile) || die $!;
+
+my $header = <$EXPR>;
+while(my $line = <$EXPR>){
+	my @lineArr = split(/\t/, $line);
+	
+	my $minExpr = 1000000000000;
+	my $maxExpr = -1;
+	my $aveExpr = -1;
+	my $totalExpr = 0;
+	for(my $i = 2; $i < scalar(@lineArr); $i++)	{
+		if($lineArr[$i] < $minExpr){
+			$minExpr = $lineArr[$i];
+		}
+		if($lineArr[$i] > $maxExpr){
+			$maxExpr = $lineArr[$i];
+		}
+		$totalExpr += $lineArr[$i];
+	}
+	$aveExpr = $totalExpr / (scalar(@lineArr) - 2);
+	
+	if($maxExpr > $maxExprAtLeast && $aveExpr > $aveExprAtLeast){
+		print join("\t", @lineArr) . "\n";
+	}
+}
+
+close($EXPR);
